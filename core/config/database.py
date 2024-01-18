36563821e6a7a140dd8de81ad1fde959d4cbe16c
Akinola Abiodun <akinolaemmanuel49@gmail.com>
2024-01-18 14:33:21 +0100

    setup env and db config

---
+++ core/config/database.py
@@ -0,0 +1,18 @@
+import pymongo
+from pymongo.mongo_client import MongoClient
+
+from core.config.env import Env
+
+env = Env()
+
+
+class AuthDatabase:
+    uri = 'mongodb+srv://' + env.MONGODB_USERNAME + ':' + \
+        env.MONGODB_PASSWORD.get_secret_value() + \
+        '@' + env.MONGODB_CLUSTER + '/?retryWrites=true&w=majority'
+    client = MongoClient(uri)
+    db = client["auth_db"]  # Set the database name to "auth_db"
+
+    auth_collection = db["auth"]  # Set the collection name to "auth"
+    # Create a unique constraint on the "email" field in the "auth" collection
+    auth_collection.create_index([("email", pymongo.ASCENDING)], unique=True)
